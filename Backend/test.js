2c1796b0c6abb024396450a7417999fd0862a332
hameedirfan <hameedirfan13@gmail.com>
2019-09-01 23:10:18 +0530

    test file comitted

---
+++ Backend/test.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 49154;
+
+app.get('/test', function (req, res) {
+    var redirectUrl = 'https://www.instamojo.com/@predicthoroscope/d49bfdfdeeb641c1aaccf6d199bc7638';
+    res.status(200).redirect(redirectUrl)
+  });
+  
+
+// launch ======================================================================
+app.listen(port);
+console.log('The Server Running in  ' + port);
